845cf4bf061c654a6fb6545be914481c3dd4c6ae
Dylan Kuzelka <Dylan@Dylans-MacBook-Pro.local>
2017-12-19 12:48:08 -0600

    Help from Ty

--- craft/config/db.php
+++ craft/config/db.php
@@ -25,3 +25,15 @@ return array(
 	'tablePrefix' => 'craft',
 
 );
+
+// return [
+//     'server'      => getenv('MYSQL_HOST')     ?: 'localhost',
+//     'database'    => getenv('MYSQL_DATABASE') ?: 'local-db-name',
+//     'user'        => getenv('MYSQL_USER')     ?: 'local-db-user',
+//     'password'    => getenv('MYSQL_PASSWORD') ?: 'local-db-password',
+//     'tablePrefix' => 'craft',
+//
+//     // to be compatible with MySQL 5.7, requires 2.6.2949 or greater
+//     'initSQLs'     => ["SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';"],
+//
+// ];
